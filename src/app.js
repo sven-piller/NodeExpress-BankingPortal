88e4229eca1b53ca83547ad8716c849b00540b29
Sven Piller <sven.piller@gmail.de>
2020-11-21 21:10:30 +0100

    Summary and Profile

--- src/app.js
+++ src/app.js
@@ -10,9 +10,48 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync(path.join(__dirname, "./json/accounts.json"), {
+    encoding: "utf-8"
+})
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, "./json/users.json"), {
+    encoding: "utf-8"
+})
+const users = JSON.parse(userData);
+
+
+
+
+
+app.get('/savings', (req, res) => {
+    res.render('account', {
+        account: accounts.savings
+    })
+})
+
+app.get('/checking', (req, res) => {
+    res.render('account', {
+        account: accounts.checking
+    })
+})
+
+app.get('/credit', (req, res) => {
+    res.render('account', {
+        account: accounts.credit
+    })
+})
+
+app.get('/profile', (req, res) => {
+    res.render('profile', {
+        user: users[0]
+    })
+})
+
 app.get('/', (req, res) => {
     res.render('index', {
-        title: "Index"
+        title: "Account Summary",
+        accounts: accounts
     })
 });
 
